3d62ebb74875437feb46c533ce5b25c4eb5527f2
M.S.T.O.P. <mstop4@gmail.com>
2025-11-30 19:06:07 -0500

    update configs for staging env

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
   return {
+    base: env.VITE_CLIENT_BASE_URL,
     server: {
       port: 3000,
       proxy: {
         "/api": {
-          target: "https://localhost:4000",
+          target: env.VITE_API_BASE_URL,
           changeOrigin: true,
           secure: true
         },
         "/socket.io/": {
-          target: "https://localhost:4000",
+          target: env.VITE_API_BASE_URL,
           changeOrigin: true,
           ws: true
         }
